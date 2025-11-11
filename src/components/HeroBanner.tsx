35d07e8d5f306b57ccda86891e05d1110ed38c99
salman abir <salmanabir28@gmail.com>
2025-11-11 22:17:07 +0600

    Correct typo in HeroBanner component for improved clarity in school name.

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -29,7 +29,7 @@ export default function HeroBanner({ onVerifyClick }: HeroBannerProps) {
 
           {/* Description */}
           <p className="text-lg md:text-xl mb-8 mx-auto text-[#333]">
-            আয়োজনে: ইব্রাহিমপুর ঈশ্বরচন্দ্র উচ্চবিদ্যালয় সম্মিলিত প্রাক্তন
+            আয়োজনে: ইব্রাহিমপুর ঈশ্বরচন্দ্র উচ্চ বিদ্যালয় সম্মিলিত প্রাক্তন
             ছাত্র পরিষদ ।
             <br />
             অনুষ্ঠানের স্থান ও তারিখ :{" "}
